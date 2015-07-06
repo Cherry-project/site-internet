132caee898d6979725e555f6c19506b01446eaf6
Sophie Bulteau <sophie.bulteau@gmail.com>
2015-07-06 14:33:45 +0200

    général

---
+++ Child/Cafe/indexCafe.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Café</title>
+		<!--BOOTSTRAP-->
+		<link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<!--<script src="bootstrap/js/jquery.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>-->
+		<link rel="stylesheet" href="indexCafe.css" />
+	    <link href="../../bootstrap/css/bootstrap.css" rel="stylesheet">
+	   <meta charset="utf-8" />
+    </head>
+    <body>
+        
+		<header>		
+		<div class="page-header">
+			<img src="../../Affichage/avatar1.png" class="avatar" alt="Image" />
+			<img src="../../Affichage/rouages.png" class="parametre" alt="Image" />
+			<img src="../../Affichage/compte.png" class="compte" alt="Image" />
+		</div>
+        </header>
+
+        
+	
+		    <div class="container-fluid">
+				<div class="row">
+				
+						<div class="col-xs-1"></div>
+						
+						<div class="col-xs-10">
+						<div class="row">
+							<div class="col-xs-5">
+							<a href=""> <img src="../image/logo/cible.png" class="imageListe" alt="Image" /></a>
+							<h1>Email</h1>
+							</div>
+							<div class="col-xs-5">
+							<a href=".php"> <img src="../image/logo/des.png" class="imageListe" alt="Image" /></a>
+							<h1>Chat</h1>
+							</div>
+						</div>	
+						
+						<div class="row">
+							<div class="col-xs-5">
+							<a href=""> <img src="../image/logo/cible.png" class="imageListe" alt="Image" /></a>
+							<h1>Skype</h1>
+							</div>
+							<div class="col-xs-5">
+							<a href=".php"> <img src="../image/logo/des.png" class="imageListe" alt="Image" /></a>
+							<h1>Prim@kid</h1>
+							</div>
+						</div>	
+						
+						</div>		
+					<div class="col-xs-1"></div>
+
+				</div>
+			</div>
+		
+                
+        <footer>
+
+        <p> <a href="../indexChild.php"> <img src="../image/logo/retour.png" class="image1" alt="Image" /></a></p>    
+
+        </footer>
+		
+		
+    </body>
+</html>
