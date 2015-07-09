a2beb7913503bafdc1f4c1f8ba4b3d4c06e9fbd7
Sophie Bulteau <sophie.bulteau@gmail.com>
2015-07-09 16:22:53 +0200

    cafe js

---
+++ Child/Cafe/indexCafe.js
@@ -0,0 +1,153 @@
+
+
+//Récupère le contenu correspondant à l'élement
+// function getContenu(elements) {
+// 	while (elements = elements.nextSibling) {
+// 		if (elements.className === 'contenucateg') {
+// 			return elements;
+// 		}
+// 	}
+// 	return false;
+// }
+
+//CHANGEMENTS DE STYLE
+
+var changement={};
+
+changement['video'] = function(id) {
+	var categ = document.querySelectorAll('.categselect'),
+    categLength = categ.length;
+    for (var i = 0 ; i < categLength ; i++) {
+    	categ[i].className="categorie";
+    }
+    var im = document.querySelectorAll('.imagecateg'),
+    imLength = im.length;
+    for (var i = 0 ; i < imLength ; i++) {
+    	im[i].className="imagesidebar";
+    }
+    var titre = document.querySelectorAll('.titrecateg'),
+    titreLength = titre.length;
+    for (var i = 0 ; i < titreLength ; i++) {
+    	titre[i].className="titresidebar";
+    }
+
+	var elem = document.getElementById('video');
+	elem.className="categselect";
+	var tit = elem.getElementsByClassName('titresidebar')[0].innerHTML;
+	var cont = elem.getElementsByClassName('contenucateg')[0].innerHTML;
+	document.getElementsByClassName('titreaffiche')[0].innerHTML = tit
+	document.getElementsByClassName('contenuaffiche')[0].innerHTML = cont	
+};
+
+changement['email'] = function(id) {
+	var categ = document.querySelectorAll('.categselect'),
+    categLength = categ.length;
+    for (var i = 0 ; i < categLength ; i++) {
+    	categ[i].className="categorie";
+    }
+    var im = document.querySelectorAll('.imagecateg'),
+    imLength = im.length;
+    for (var i = 0 ; i < imLength ; i++) {
+    	im[i].className="imagesidebar";
+    }
+    var titre = document.querySelectorAll('.titrecateg'),
+    titreLength = titre.length;
+    for (var i = 0 ; i < titreLength ; i++) {
+    	titre[i].className="titresidebar";
+    }
+
+	var elem = document.getElementById('email');
+	elem.className="categselect";
+	var tit = elem.getElementsByClassName('titresidebar')[0].innerHTML;
+	var cont = elem.getElementsByClassName('contenucateg')[0].innerHTML;
+	document.getElementsByClassName('titreaffiche')[0].innerHTML = tit
+	document.getElementsByClassName('contenuaffiche')[0].innerHTML = cont	
+
+};
+
+changement['chat'] = function(id) {
+	categ = document.querySelectorAll('.categselect'),
+    categLength = categ.length;
+    for (var i = 0 ; i < categLength ; i++) {
+    	categ[i].className="categorie";
+    }
+    var im = document.querySelectorAll('.imagecateg'),
+    imLength = im.length;
+    for (var i = 0 ; i < imLength ; i++) {
+    	im[i].className="imagesidebar";
+    }
+    var titre = document.querySelectorAll('.titrecateg'),
+    titreLength = titre.length;
+    for (var i = 0 ; i < titreLength ; i++) {
+    	titre[i].className="titresidebar";
+    }
+
+	var elem = document.getElementById('chat');
+	elem.className="categselect";
+	var tit = elem.getElementsByClassName('titresidebar')[0].innerHTML;
+	var cont = elem.getElementsByClassName('contenucateg')[0].innerHTML;
+	document.getElementsByClassName('titreaffiche')[0].innerHTML = tit
+	document.getElementsByClassName('contenuaffiche')[0].innerHTML = cont	
+
+};
+
+changement['primakid'] = function(id) {
+	categ = document.querySelectorAll('.categselect'),
+    categLength = categ.length;
+    for (var i = 0 ; i < categLength ; i++) {
+    	categ[i].className="categorie";
+    }
+    var im = document.querySelectorAll('.imagecateg'),
+    imLength = im.length;
+    for (var i = 0 ; i < imLength ; i++) {
+    	im[i].className="imagesidebar";
+    }
+    var titre = document.querySelectorAll('.titrecateg'),
+    titreLength = titre.length;
+    for (var i = 0 ; i < titreLength ; i++) {
+    	titre[i].className="titresidebar";
+    }
+
+	var elem = document.getElementById('primakid');
+	elem.className="categselect";
+	var tit = elem.getElementsByClassName('titresidebar')[0].innerHTML;
+	var cont = elem.getElementsByClassName('contenucateg')[0].innerHTML;
+	document.getElementsByClassName('titreaffiche')[0].innerHTML = tit
+	document.getElementsByClassName('contenuaffiche')[0].innerHTML = cont	
+
+};
+
+
+///FONCTION DE BASE
+(function() { // Utilisation d'une IIFE pour éviter les variables globales.
+
+    //var myForm = document.getElementById('myForm'),
+    categ = document.querySelectorAll('.imagecateg'),
+    categLength = categ.length;
+
+    for (var i = 0 ; i < categLength ; i++) {
+    	categ[i].addEventListener('click', function(e) {
+
+    		var affiche = document.querySelectorAll('.contafficheFALSE'),
+    		afficheLength = affiche.length;
+    		for (var i = 0 ; i < afficheLength ; i++) {
+    			affiche[i].className="contafficheTRUE";
+    		
+
+    		var encadre = document.querySelectorAll('.encadre_def'),
+    		encadreLength = encadre.length;
+    		for (var i = 0 ; i < encadreLength ; i++) {
+    			encadre[i].className="encadre_sidebar";
+    		}
+
+    		var encadre = document.querySelectorAll('.categorie_def'),
+    		encadreLength = encadre.length;
+    		for (var i = 0 ; i < encadreLength ; i++) {
+    			encadre[i].className="categorie";
+    		}
+
+    		cat=e.target.parentNode;
+    		changement[cat.id](cat.id); // "e.target" représente l'input actuellement modifié
+    	}, false);
+    }
+})();   
--- Child/Cafe/indexCafe.php
+++ Child/Cafe/indexCafe.php
@@ -1,67 +1,108 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Café</title>
-		<!--BOOTSTRAP-->
-		<link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<!--<script src="bootstrap/js/jquery.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>-->
-		<link rel="stylesheet" href="indexCafe.css" />
-	    <link href="../../bootstrap/css/bootstrap.css" rel="stylesheet">
-	   <meta charset="utf-8" />
-    </head>
-    <body>
-        
-		<header>		
+<head>
+    <title>Café</title>
+	<!--BOOTSTRAP-->
+	<link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!--<script src="bootstrap/js/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>-->
+	<link rel="stylesheet" href="indexCafe.css" />
+	<link href="../../bootstrap/css/bootstrap.css" rel="stylesheet">
+	<meta charset="utf-8" />
+</head>
+
+<body>
+    <header>		
 		<div class="page-header">
 			<img src="../../Affichage/avatar1.png" class="avatar" alt="Image" />
 			<img src="../../Affichage/rouages.png" class="parametre" alt="Image" />
 			<img src="../../Affichage/compte.png" class="compte" alt="Image" />
 		</div>
-        </header>
+    </header>
 
-        
+ 	<div class="encadre_def">  
+
+	<div id="video" class="categorie_def">
+		<img src="../image/logo/webcam.png" class="imagecateg" alt="Image" />
+		<h1 class = "titrecateg">Chat vidéo</h1>
+		<div class="contenucateg"> 
+			<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat quam sapien, nec venenatis enim ullamcorper ac. Nam luctus ex enim, in tristique nibh ullamcorper quis. Phasellus ac risus nec velit rhoncus vulputate nec in lacus. Quisque commodo nec nibh vitae maximus. Nam consectetur convallis tellus ac venenatis. Etiam rutrum lobortis tempus. Donec sit amet libero non magna sollicitudin molestie sit amet sit amet nisi. Ut nec mi risus. Vivamus non sapien est. Quisque sed finibus massa. Nulla facilisi.
+
+					Nam quis nulla ac augue porttitor elementum. Vestibulum id dapibus neque. Proin sit amet urna rhoncus, scelerisque odio sed, mollis orci. Suspendisse suscipit tristique turpis sed semper. Vestibulum porttitor orci sapien, nec gravida tortor aliquet ac. Integer hendrerit ornare ipsum tempor porttitor. Donec velit arcu, tempus eu sem ut, blandit bibendum dui. Donec tempus sed augue sed bibendum. Cras sollicitudin urna sed felis volutpat, nec ultricies dolor congue. In velit nulla, laoreet nec tristique vitae, hendrerit in lacus. Morbi finibus eros ut arcu efficitur, lobortis vulputate ipsum congue.
+
+					Nam consequat ante et nisi fermentum, in bibendum ligula tincidunt. Suspendisse cursus ligula at metus venenatis, et feugiat lectus facilisis. Integer vel mollis enim. Maecenas pulvinar massa mattis, fermentum enim vitae, pharetra metus. Phasellus a dui tincidunt, commodo arcu et, interdum justo. Sed orci magna, finibus sed neque at, tristique scelerisque lectus. Sed tincidunt magna ut maximus consequat. Cras augue mi, mollis quis ante et, feugiat mattis diam. Duis feugiat pretium luctus.
+
+					Fusce dictum est orci, in suscipit libero cursus mollis. Integer eget magna quam. Nunc id pellentesque lacus. Proin pulvinar turpis sit amet turpis placerat, a pretium purus porttitor. Aenean sit amet arcu at ex interdum auctor eleifend ut ante. Sed nec odio eget felis faucibus imperdiet. Donec ac nulla justo.  </p> 
+		</div>
+	</div>
+	<div id="email" class="categorie_def">
+		<img src="../image/logo/email.png" class="imagecateg" alt="Image" />
+		<h1 class = "titrecateg">Mes emails</h1>
+		<div class="contenucateg"> 
+			<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat quam sapien, nec venenatis enim ullamcorper ac. Nam luctus ex enim, in tristique nibh ullamcorper quis. Phasellus ac risus nec velit rhoncus vulputate nec in lacus. Quisque commodo nec nibh vitae maximus. Nam consectetur convallis tellus ac venenatis. Etiam rutrum lobortis tempus. Donec sit amet libero non magna sollicitudin molestie sit amet sit amet nisi. Ut nec mi risus. Vivamus non sapien est. Quisque sed finibus massa. Nulla facilisi.
+
+					Nam quis nulla ac augue porttitor elementum. Vestibulum id dapibus neque. Proin sit amet urna rhoncus, scelerisque odio sed, mollis orci. Suspendisse suscipit tristique turpis sed semper. Vestibulum porttitor orci sapien, nec gravida tortor aliquet ac. Integer hendrerit ornare ipsum tempor porttitor. Donec velit arcu, tempus eu sem ut, blandit bibendum dui. Donec tempus sed augue sed bibendum. Cras sollicitudin urna sed felis volutpat, nec ultricies dolor congue. In velit nulla, laoreet nec tristique vitae, hendrerit in lacus. Morbi finibus eros ut arcu efficitur, lobortis vulputate ipsum congue.
+
+					Nam consequat ante et nisi fermentum, in bibendum ligula tincidunt. Suspendisse cursus ligula at metus venenatis, et feugiat lectus facilisis. Integer vel mollis enim. Maecenas pulvinar massa mattis, fermentum enim vitae, pharetra metus. Phasellus a dui tincidunt, commodo arcu et, interdum justo. Sed orci magna, finibus sed neque at, tristique scelerisque lectus. Sed tincidunt magna ut maximus consequat. Cras augue mi, mollis quis ante et, feugiat mattis diam. Duis feugiat pretium luctus.
+
+					Fusce dictum est orci, in suscipit libero cursus mollis. Integer eget magna quam. Nunc id pellentesque lacus. Proin pulvinar turpis sit amet turpis placerat, a pretium purus porttitor. Aenean sit amet arcu at ex interdum auctor eleifend ut ante. Sed nec odio eget felis faucibus imperdiet. Donec ac nulla justo. </p> 
+		</div>
+	</div>
+	<div id="chat" class="categorie_def">
+		<img src="../image/logo/bulle.png" class="imagecateg" alt="Image" />
+		<h1 class = "titrecateg">Chat</h1>
+		<div class="contenucateg"> 
+			<img src="../image/logo/bubbles.png" alt="Image" />
+			<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat quam sapien, nec venenatis enim ullamcorper ac. Nam luctus ex enim, in tristique nibh ullamcorper quis. Phasellus ac risus nec velit rhoncus vulputate nec in lacus. Quisque commodo nec nibh vitae maximus. Nam consectetur convallis tellus ac venenatis. Etiam rutrum lobortis tempus. Donec sit amet libero non magna sollicitudin molestie sit amet sit amet nisi. Ut nec mi risus. Vivamus non sapien est. Quisque sed finibus massa. Nulla facilisi.
+
+				Nam quis nulla ac augue porttitor elementum. Vestibulum id dapibus neque. Proin sit amet urna rhoncus, scelerisque odio sed, mollis orci. Suspendisse suscipit tristique turpis sed semper. Vestibulum porttitor orci sapien, nec gravida tortor aliquet ac. Integer hendrerit ornare ipsum tempor porttitor. Donec velit arcu, tempus eu sem ut, blandit bibendum dui. Donec tempus sed augue sed bibendum. Cras sollicitudin urna sed felis volutpat, nec ultricies dolor congue. In velit nulla, laoreet nec tristique vitae, hendrerit in lacus. Morbi finibus eros ut arcu efficitur, lobortis vulputate ipsum congue.
+
+				Nam consequat ante et nisi fermentum, in bibendum ligula tincidunt. Suspendisse cursus ligula at metus venenatis, et feugiat lectus facilisis. Integer vel mollis enim. Maecenas pulvinar massa mattis, fermentum enim vitae, pharetra metus. Phasellus a dui tincidunt, commodo arcu et, interdum justo. Sed orci magna, finibus sed neque at, tristique scelerisque lectus. Sed tincidunt magna ut maximus consequat. Cras augue mi, mollis quis ante et, feugiat mattis diam. Duis feugiat pretium luctus.
+
+				Fusce dictum est orci, in suscipit libero cursus mollis. Integer eget magna quam. Nunc id pellentesque lacus. Proin pulvinar turpis sit amet turpis placerat, a pretium purus porttitor. Aenean sit amet arcu at ex interdum auctor eleifend ut ante. Sed nec odio eget felis faucibus imperdiet. Donec ac nulla justo.  </p> 
+		</div>
+	</div>
+	<div id ="primakid" class="categorie_def">
+		<img src="../image/logo/webcam.png" class="imagecateg" alt="Image" />
+		<h1 class = "titrecateg">Primakid</h1>
+		<div class="contenucateg"> 
+			<img src="../image/logo/prix.png" alt="Image" />
+			</br>
+			<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat quam sapien, nec venenatis enim ullamcorper ac. Nam luctus ex enim, in tristique nibh ullamcorper quis. Phasellus ac risus nec velit rhoncus vulputate nec in lacus. Quisque commodo nec nibh vitae maximus. Nam consectetur convallis tellus ac venenatis. Etiam rutrum lobortis tempus. Donec sit amet libero non magna sollicitudin molestie sit amet sit amet nisi. Ut nec mi risus. Vivamus non sapien est. Quisque sed finibus massa. Nulla facilisi.
+
+				Nam quis nulla ac augue porttitor elementum. Vestibulum id dapibus neque. Proin sit amet urna rhoncus, scelerisque odio sed, mollis orci. Suspendisse suscipit tristique turpis sed semper. Vestibulum porttitor orci sapien, nec gravida tortor aliquet ac. Integer hendrerit ornare ipsum tempor porttitor. Donec velit arcu, tempus eu sem ut, blandit bibendum dui. Donec tempus sed augue sed bibendum. Cras sollicitudin urna sed felis volutpat, nec ultricies dolor congue. In velit nulla, laoreet nec tristique vitae, hendrerit in lacus. Morbi finibus eros ut arcu efficitur, lobortis vulputate ipsum congue.
+
+				Nam consequat ante et nisi fermentum, in bibendum ligula tincidunt. Suspendisse cursus ligula at metus venenatis, et feugiat lectus facilisis. Integer vel mollis enim. Maecenas pulvinar massa mattis, fermentum enim vitae, pharetra metus. Phasellus a dui tincidunt, commodo arcu et, interdum justo. Sed orci magna, finibus sed neque at, tristique scelerisque lectus. Sed tincidunt magna ut maximus consequat. Cras augue mi, mollis quis ante et, feugiat mattis diam. Duis feugiat pretium luctus.
+
+				Fusce dictum est orci, in suscipit libero cursus mollis. Integer eget magna quam. Nunc id pellentesque lacus. Proin pulvinar turpis sit amet turpis placerat, a pretium purus porttitor. Aenean sit amet arcu at ex interdum auctor eleifend ut ante. Sed nec odio eget felis faucibus imperdiet. Donec ac nulla justo.  </p> 
+			<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat quam sapien, nec venenatis enim ullamcorper ac. Nam luctus ex enim, in tristique nibh ullamcorper quis. Phasellus ac risus nec velit rhoncus vulputate nec in lacus. Quisque commodo nec nibh vitae maximus. Nam consectetur convallis tellus ac venenatis. Etiam rutrum lobortis tempus. Donec sit amet libero non magna sollicitudin molestie sit amet sit amet nisi. Ut nec mi risus. Vivamus non sapien est. Quisque sed finibus massa. Nulla facilisi.
+
+				Nam quis nulla ac augue porttitor elementum. Vestibulum id dapibus neque. Proin sit amet urna rhoncus, scelerisque odio sed, mollis orci. Suspendisse suscipit tristique turpis sed semper. Vestibulum porttitor orci sapien, nec gravida tortor aliquet ac. Integer hendrerit ornare ipsum tempor porttitor. Donec velit arcu, tempus eu sem ut, blandit bibendum dui. Donec tempus sed augue sed bibendum. Cras sollicitudin urna sed felis volutpat, nec ultricies dolor congue. In velit nulla, laoreet nec tristique vitae, hendrerit in lacus. Morbi finibus eros ut arcu efficitur, lobortis vulputate ipsum congue.
+
+				Nam consequat ante et nisi fermentum, in bibendum ligula tincidunt. Suspendisse cursus ligula at metus venenatis, et feugiat lectus facilisis. Integer vel mollis enim. Maecenas pulvinar massa mattis, fermentum enim vitae, pharetra metus. Phasellus a dui tincidunt, commodo arcu et, interdum justo. Sed orci magna, finibus sed neque at, tristique scelerisque lectus. Sed tincidunt magna ut maximus consequat. Cras augue mi, mollis quis ante et, feugiat mattis diam. Duis feugiat pretium luctus.
+
+				Fusce dictum est orci, in suscipit libero cursus mollis. Integer eget magna quam. Nunc id pellentesque lacus. Proin pulvinar turpis sit amet turpis placerat, a pretium purus porttitor. Aenean sit amet arcu at ex interdum auctor eleifend ut ante. Sed nec odio eget felis faucibus imperdiet. Donec ac nulla justo.  </p> 
+
+		</div>
+	</div>
 	
-		    <div class="container-fluid">
-				<div class="row">
-				
-						<div class="col-xs-1"></div>
-						
-						<div class="col-xs-10">
-						<div class="row">
-							<div class="col-xs-5">
-							<a href=""> <img src="../image/logo/cible.png" class="imageListe" alt="Image" /></a>
-							<h1>Email</h1>
-							</div>
-							<div class="col-xs-5">
-							<a href=".php"> <img src="../image/logo/des.png" class="imageListe" alt="Image" /></a>
-							<h1>Chat</h1>
-							</div>
-						</div>	
-						
-						<div class="row">
-							<div class="col-xs-5">
-							<a href=""> <img src="../image/logo/cible.png" class="imageListe" alt="Image" /></a>
-							<h1>Skype</h1>
-							</div>
-							<div class="col-xs-5">
-							<a href=".php"> <img src="../image/logo/des.png" class="imageListe" alt="Image" /></a>
-							<h1>Prim@kid</h1>
-							</div>
-						</div>	
-						
-						</div>		
-					<div class="col-xs-1"></div>
 
-				</div>
-			</div>
-		
-                
-        <footer>
+	</div>
 
-        <p> <a href="../indexChild.php"> <img src="../image/logo/retour.png" class="image1" alt="Image" /></a></p>    
+	<div class="contafficheFALSE">
+		<h1 class = "titreaffiche"></h1>
+		<div class="contenuaffiche">
+		</div>
+	</div>
 
-        </footer>
+    <script src="indexCafe.js" ></script>   
+
+
+    <footer>
+
+    <p> <a href="../indexChild.php"> <img src="../image/logo/retour.png" class="image1" alt="Image" /></a></p>    
+
+    </footer>
 		
 		
     </body>
