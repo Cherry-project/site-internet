3e53c4f0c0b33636c05c99a60cc342f27a4c0c06
Sophie Bulteau <sophie.bulteau@gmail.com>
2015-07-15 09:30:26 +0200

    externalisation head/foot

---
+++ Child/header_footer/footer.php
@@ -0,0 +1,7 @@
+    <link rel="stylesheet" href="../header_footer/footer.css" />
+
+    <footer>
+
+    <p> <a href="../indexChild.php"> <img src="../image/logo/retour.png" class="image1" alt="Retour Village" /></a></p>    
+
+    </footer>
