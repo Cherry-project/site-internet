0879632c1888bbdc1656659ce2d4079daa2af7e7
NicoDev <nrichard@neosoft-sas.com>
2015-12-18 10:22:50 +0100

    [Bug drop.php] log out activé quand on est connecté

--- CherryUnity/drop.php
+++ CherryUnity/drop.php
@@ -23,35 +23,7 @@ session_start();
       $children = $childDao->getChildren($email);
       ?>
       
-
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Projet Cherry</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="#">Log in
-		<span class="glyphicon glyphicon-log-in"></span></a></li>
-            
-            <li ><a href="Authentication.html">Sign up <span class="glyphicon glyphicon-user"></a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div><!--/.container-fluid -->
-    </nav>
-
+      <?php include 'nav.php' ?>
       
       <a href="./adultShowContents.php">Liste des fichiers</a>
 
