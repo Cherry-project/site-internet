0c4eece143c075018a8dcbcdff8075141c6add34
NicoDev <nrichard@neosoft-sas.com>
2015-12-18 10:13:35 +0100

    [Achievements] refactor du code

--- CherryUnity/avatarWebService.php
+++ CherryUnity/avatarWebService.php
@@ -2,24 +2,24 @@
 $root = "./";
 include 'includes.php'; 
 
-
 $Adao = new AchievementsDAO(DynamoDbClientBuilder::get());
+
 if(!empty($_GET['email'])){
-$email = $_GET['email'];
-if(empty($_GET['avatar'])){
-$avatar = $Adao->getTutorial($email);
+    $email = $_GET['email'];
+    if(empty($_GET['avatar'])){
+        $avatar = $Adao->getTutorial($email);
 
-header('Content-Type: application/json');
+        header('Content-Type: application/json');
 
-$response = json_encode(array($avatar));
-echo $response;
-}
-else{
-    $avatar = $_GET['avatar'];
-    echo $avatar;
-    $Adao->updateAvatar($email, $avatar);
-    //echo 'a';
-}
+        $response = json_encode(array($avatar));
+        echo $response;
+    }
+    else{
+        $avatar = $_GET['avatar'];
+        echo $avatar;
+        $Adao->updateAvatar($email, $avatar);
+        //echo 'a';
+    }
 }
 ?>
 
