46d4cb2167fdf313282c8b146f065d42aa41df44
AdelineMory <adeline.mory@ensc.fr>
2016-04-04 09:38:06 +0200

    Added files via upload

--- CherryUnity/footer.php
+++ CherryUnity/footer.php
@@ -4,9 +4,9 @@ if (empty($root)) {
 }
 ?>
 
-<footer class="footer">
+<footer class="footer" height= 37px;>
     <div class="container-fluid">
-        <img  height="60px" src=<?php echo $root."img/logo.jpg" ?> /> 
+        <img  height="33px" src=<?php echo $root."img/logo.jpg" ?> /> 
     </div>
 </footer>
 
--- CherryUnity/index.php
+++ CherryUnity/index.php
@@ -1,9 +1,8 @@
-<?php session_start()
- // Désactiver le rapport d'erreurs
-    error_reporting(0); ?>
+<?php error_reporting(0);
+    session_start();?>
 <!doctype html>
 <html>
-
+    
 <head>
     <?php include 'head.php' ?>
     <title>Connection </title>
@@ -11,8 +10,8 @@
 
 <body>
     <?php include 'nav.php' ?>
-
-
+    
+    
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-md-offset-3">
@@ -26,15 +25,13 @@
                         <input id="password" name="password" class="form-control" placeholder="Entrez un mot de passe" type="password">
                     </div>
                     <button type="submit" class="btn btn-default">Se connecter</button>
-					<!--deplacement du bouton "S"inscrire"-->
-					<a href="authentication.php"><button  class="btn btn-primary">S'inscrire</button></a>
                 </form>
             </div>
         </div>
         <div class="row">
             <div class="col-md-3 col-md-offset-3">
                 <a href="forgotPassword.php">Mot de passe oublié? </a>
-                <!--ancienne place du bouton "S'inscrire"-->
+                <a href="authentication.php"><button  class="btn btn-primary">S'inscrire</button></a>
             </div>
         </div>
     </div>
